3b48aa7e2607436cf52bae79a5a059333c0a2df0
Andy Knolla <andyknolla@gmail.com>
2016-07-12 14:00:28 -0600

    add forms for authors and books

---
+++ README.md
@@ -0,0 +1,1 @@
+site url: https://g-reads-ak.herokuapp.com/
--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('authors', {word:'dirka'})
+  res.render('authors', {word:'stuff'})
 });
 
 module.exports = router;
