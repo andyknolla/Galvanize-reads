190bb38b1b94c74285149c5269c8ca8f98525293
Andy Knolla <andyknolla@gmail.com>
2016-07-13 10:35:53 -0600

    Complete edit-author feature

--- routes/authors.js
+++ routes/authors.js
@@ -5,11 +5,25 @@ var knex = require('knex')
 var queries = require('../db/queries')
 
 router.get('/', function(req, res, next) {
- queries.getAuthors().then(function(data) {
-  res.render('authors', { authors: data })
-  })
+    queries.getAuthors().then(function(data) {
+        res.render('authors', {
+            authors: data
+        })
+    })
 });
-// add an author
+
+// go to author detail page
+router.get('/author_detail/:id', function(req, res, next) {
+        queries.getAuthors(req.body).where({
+                'id': req.params.id
+            }).first()
+            .then(function(data) {
+                res.render('author_detail', {
+                    author: data
+                })
+            })
+    })
+    // add an author
 router.post('/add_author', function(req, res, next) {
         console.log('add author post route hits');
         queries.addAuthor(req.body).then(function(data) {
@@ -18,28 +32,28 @@ router.post('/add_author', function(req, res, next) {
     })
     // Delete an author
 router.get('/delete_author/:id', function(req, res, next) {
-        console.log('delete author route hits');
-        queries.getAuthors().where({
-            'id': req.params.id
-        }).del().then(function() {
-            res.redirect('/authors')
-        })
+    console.log('delete author route hits');
+    queries.getAuthors().where({
+        'id': req.params.id
+    }).del().then(function() {
+        res.redirect('/authors')
     })
+})
 
-    // Edit an author
+// Edit an author
 router.get('/edit_author/:id', function(req, res, next) {
-        console.log('edit author route hits')
-        queries.getAuthors().where({
-            'id': req.params.id
-        }).first().then(function(data) {
-            console.log(data);
-            res.render('edit_author', {
-                author: data
-            })
+    console.log('edit author route hits')
+    queries.getAuthors().where({
+        'id': req.params.id
+    }).first().then(function(data) {
+        console.log(data);
+        res.render('edit_author', {
+            author: data
         })
     })
+})
 router.post('/edit_author/:id', function(req, res, next) {
-    queries.getAuthor().where({
+    queries.getAuthors().where({
             'id': req.params.id
         })
         .update(req.body)
